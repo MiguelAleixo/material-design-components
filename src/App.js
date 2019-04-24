af70890b23e16cbba66b9ad16cfcfeadf9ef308a
Miguel Aleixo <miguelgomes2099@hotmail.com>
2019-04-24 12:16:24 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Home from './main/home/Home'
+
+function App() {
+  // render() {
+    return (
+      <div>
+        <h1> App </h1>
+        <Home/>
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+  // }
+}
+
+export default App;
